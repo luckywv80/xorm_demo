006dd3aa984c887926b5f5c4bb5780bc3f56a4b5
陈鹏杰 <260083304@qq.com>
2019-04-03 17:28:46 +0800

    init

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 	_ "github.com/lunny/godbc"
 	_ "github.com/mattn/go-oci8"
 
-	"fmt"
+	"log"
+	"os"
 )
 
 type DbType struct {
@@ -31,19 +32,16 @@ var dbType = []DbType{
 	{DbName: "oci8", DbSource: "root/123@192.168.0.120:1521/ORCL"},
 }
 
-var index  = 0  //mysql
-//var index  = 0   //mssql
-//var index  = 0  //oci8
+//var index  = 0  //mysql
+//var index  = 1   //mssql
+var index  = 2  //oci8
 func main() {
 
 
-	fmt.Println("启动数据库数据库引擎 ",dbType[index].DbName)
+	log.Println("启动数据库数据库引擎 ",dbType[index].DbName)
 	Engine, err := xorm.NewEngine(dbType[index].DbName, dbType[index].DbSource)
+	assert(err,"连接引擎出错")
 
-	if err != nil {
-		fmt.Println("连接引擎出错 : ", err)
-		return
-	}
 
 	Engine.SetTableMapper(core.SameMapper{})
 	Engine.SetColumnMapper(core.SameMapper{})
@@ -52,11 +50,43 @@ func main() {
 	Engine.SetMaxIdleConns(5)
 
 	config :=new(SYS_CONFIG)
-	if _,err=Engine.Get(config);err!=nil {
-		fmt.Println(" get is error ",err)
+
+	 _,err=Engine.Get(config)
+	assert(err," get is error ")
+
+	log.Println("通过结构体获取 : ",config,"\n\n")
+
+	//获取单条
+	config1  := make(map[string]string)
+	//_,err = Engine.Table("SYS_CONFIG").Get(&config1)
+	_,err = Engine.Table(new(SYS_CONFIG)).Get(&config1)
+	assert(err,"通过数据错误报错 ")
+	log.Println("通过条件获取 :",config1,"\n\n")
+
+	//获取多条
+	config2 :=make([]map[string]string,0)
+	err=Engine.Table(new(SYS_CONFIG)).Find(&config2)
+	assert(err,"获取多条报错 ")
+	log.Println("获取多条",config2,"\n\n")
+
+	//非struct的单个字段的数组
+	cfgId :=make([]string,0)
+	err = Engine.Table(new(SYS_CONFIG)).Cols("CFG_ID").Find(&cfgId)
+	assert(err,"获取个别字段出错  ")
+	log.Println("获取个别字段 ",cfgId,"\n\n")
+
+	//非struct的多个字段的数组
+	cfgIdList :=make([][]string,0)
+	err = Engine.Table(new(SYS_CONFIG)).Cols("CFG_ID","CFG_TYPE").Find(&cfgIdList)
+	assert(err,"获取多个字段出错  ")
+	log.Println("获取多别字段 ",cfgIdList,"\n\n")
+
+}
+
+
+func assert(err error,msg string){
+	if err != nil {
+		log.Println(msg,err)
+		os.Exit(0)
 	}
-	fmt.Println(config)
-
-
-
 }
